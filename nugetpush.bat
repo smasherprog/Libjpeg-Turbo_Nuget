497a81cc456777ae1c591a4a7e953194260caad2
Scott <smasherprog@gmail.com>
2016-12-19 06:04:53 -0800

    local changes

--- nugetpush.bat
+++ nugetpush.bat
@@ -3,6 +3,9 @@ call del *.nupkg
 set dstnuget=libjpeg_turbo_nuget
 copy Libjpeg-Turbo.targets %dstnuget%\build\native\
 copy Libjpeg-Turbo.nuspec %dstnuget%\
+
 nuget pack %dstnuget%\Libjpeg-Turbo.nuspec
-for /r . %%g in (*.nupkg) do nuget push %%g -Timeout 2147483
+
+for /r . %%g in (*.nupkg) do nuget push -Source https://www.nuget.org %%g -Timeout 2147483
 call del *.nupkg
+
